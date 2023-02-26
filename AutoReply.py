837a970893f36b483ac473b26918e0e6f869f883
Qiuyelin <85266337+pooneyy@users.noreply.github.com>
2023-02-26 20:06:16 +0800

    Update AutoReply.py

--- AutoReply.py
+++ AutoReply.py
@@ -10,7 +10,7 @@ from typing import BinaryIO , Dict , List , Union
 import base64
 import logging.config ,sys
 
-__verison__ = "0.23.02.24.0"
+__verison__ = "0.23.02.26.0"
 
 def outputLog(projectName):
     log = logging.getLogger(f"{projectName}")
