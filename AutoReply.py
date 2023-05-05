4b2cbb10643003947772f065ded1ba599d000027
0honus0 <63273720+0honus0@users.noreply.github.com>
2023-05-05 18:01:46 +0800

    修改版本号

--- AutoReply.py
+++ AutoReply.py
@@ -10,7 +10,7 @@ from typing import BinaryIO , Dict , List , Union
 import base64
 import logging.config ,sys
 
-__verison__ = "0.23.04.11.1"
+__verison__ = "0.23.05.05.1"
 
 def outputLog(projectName):
     log = logging.getLogger(f"{projectName}")
